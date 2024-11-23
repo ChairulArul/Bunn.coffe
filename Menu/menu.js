a7c74b7278a60dd14ff5ace47df795c9fdaac8c2
chairul <chairul@students.mikroskil.ac.id>
2024-11-23 07:14:31 +0700

    melakukan perbaikan dan fitur baru pada web

---
+++ Menu/menu.js
@@ -0,0 +1,11 @@
+document.getElementById("menu1").addEventListener("click", function () {
+  window.location.href = "Menu/menu1.html";
+});
+
+document.getElementById("menu2").addEventListener("click", function () {
+  window.location.href = "Menu/menu2.html";
+});
+
+document.getElementById("menu3").addEventListener("click", function () {
+  window.location.href = "Menu/menu3.html";
+});
